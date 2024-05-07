e91558e281d61b45612c1cf13762db28cb5abb30
brian Céspedes aguilera <cespedes.aguilera.brian@gmail.com>
2024-05-07 20:47:09 -0300

    create module users with comand

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { tasksModule } from "./tasks/tasks.module";
 import { ProjectsModule } from './projects/projects.module';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [ tasksModule, ProjectsModule, AuthModule ],
+  imports: [ tasksModule, ProjectsModule, AuthModule, UsersModule ],
 })
 export class AppModule {}
