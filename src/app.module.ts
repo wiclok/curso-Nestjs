9a6fc6cee16fab895cec9636a67e40d25a3a462b
brian Céspedes aguilera <cespedes.aguilera.brian@gmail.com>
2024-05-07 20:40:47 -0300

    connection tasks.module.ts with app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
+import { tasksModule } from "./tasks/tasks.module";
 
 @Module({
-  imports: [],
+  imports: [ tasksModule ],
 })
 export class AppModule {}
