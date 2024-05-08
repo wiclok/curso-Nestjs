68f18c7f06e1899521c25f559d3510724682a23e
brian Céspedes aguilera <cespedes.aguilera.brian@gmail.com>
2024-05-07 21:39:03 -0300

    add CRUD of task.controller.ts

--- src/tasks/task.controller.ts
+++ src/tasks/task.controller.ts
@@ -1,5 +1,5 @@
 import { TaskService } from './task.service';
-import { Controller, Get } from "@nestjs/common";
+import { Controller, Get, Post, Put, Delete, Patch } from "@nestjs/common";
 
 @Controller({})
 export class taskController{
@@ -8,9 +8,26 @@ export class taskController{
 
   @Get('/tasks')
   getAllTasks(){
-
     return this.taskService.getTasks();
+  }
 
+  @Post('tasks')
+  createTask(){
+    return 'Creando Tareas...'
+  }
+
+  @Put('/tasks')
+  updateTask(){
+    return 'Editando Tareas...'
+  }
+
+  @Delete()
+  deleteTask(){
+    return 'Elimando Tareas...'
+  }
+  @Patch()
+  updateTaskStatus(){
+    return 'Editando el estado de la tareas...'
   }
 
 }
